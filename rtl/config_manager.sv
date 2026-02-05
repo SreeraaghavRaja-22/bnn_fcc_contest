19e21600694043dbd09bf7ed14a12daebeaf92af
Sreeraaghav-22 <rajas@ufl.edu>
2026-02-05 15:59:31 -0500

    added config manager definitions

---
+++ rtl/config_manager.sv
@@ -0,0 +1,19 @@
+module config_manager
+#(parameter int BUS_WIDTH,
+  parameter int LAYERS,
+  parameter int PARALLEL_INPUTS, 
+  parameter int PARALLEL_NEURONS
+)(
+    input logic clk, 
+    input logic rst, 
+    input logic config_data,
+    input logic config_valid, 
+    input logic config_keep,
+    input logic config_last, 
+    output logic config_ready, 
+    output logic weight_ram_wr_data, 
+    output logic weight_ram_wr_en, 
+    output logic threshold_ram_wr_data,
+    output logic threshold_ram_wr_en
+);
+endmodule
