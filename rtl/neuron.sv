3304463c70b3b7470b9d405094379eabe17ff666
jackie-giyu <wjackiemath@gmail.com>
2026-02-12 17:31:50 -0500

    Neuron Started

---
+++ rtl/neuron.sv
@@ -0,0 +1,15 @@
+module neuron #(
+    parameter NUM_WEIGHTS = 67,
+    parameter NUM_INPUTS = 67,
+    parameter THRESHOLD_BITS = 32
+)
+(
+    input logic [NUM_WEIGHTS-1:0] w,
+    input logic [NUM_INPUTS-1:0] x,
+    input logic [THRESHOLD_BITS-1:0] threshold,
+);
+
+  logic [NUM_WEIGHTS-1:0] xnor_block_out;
+  assign xnor_block_out = w ~^ x;
+
+endmodule
