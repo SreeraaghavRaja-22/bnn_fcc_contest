68ce0ce34c508514fdc589f28cc08204555c3b6d
Sreeraaghav-22 <rajas@ufl.edu>
2026-02-13 14:44:51 -0500

    struggling to understand waht to do

--- rtl/neuron.sv
+++ rtl/neuron.sv
@@ -1,22 +1,33 @@
 module neuron #(
-	parameter NUM_WEIGHTS = 67,
-	parameter NUM_INPUTS = 67,
-	parameter THRESHOLD_BITS = 32
+	parameter NUM_WEIGHTS = 4,
+	parameter NUM_INPUTS = 4,
+	parameter THRESHOLD_BITS = 3
 )
 (
+	input logic clk, 
+	input logic rst, 
+	input logic en, 
 	input logic [NUM_WEIGHTS-1:0] w,
 	input logic [NUM_INPUTS-1:0] x,
-	input logic [THRESHOLD_BITS-1:0] threshold
+	input logic [THRESHOLD_BITS-1:0] threshold,
+	output logic [$clog2(NUM_WEIGHTS+1)-1:0] y
 );
 
-	logic [NUM_WEIGHTS-1:0] xnor_block_out;
-	logic [$clog2(NUM_WEIGHTS+1)-1:0] count_ones_out;
+	logic [NUM_INPUTS-1:0] xnor_out, xnor_out_r; 
+	logic [$clog2(NUM_WEIGHTS+1)-1:0] count_ones_out, count_ones_out_r;
+	logic w1_r, w2_r;
 
-	assign xnor_block_out = w ~^ x;
-	assign count_ones_out = $countones(xnor_block_out);
+	assign xnor_out = w ~^ x;
+	// assign count_ones_out = $countones(xnor_block_out); // popcount
 
-	always_comb begin 
-		
+	// adding the inputs and weights done here (pipeline this)
+
+	always_ff @(posedge clk or posedge rst) begin // first implement with a basic pipeline 
+		if (rst) begin
+			xnor_out_r <= 1'b0;
+		end
 	end
+
+	assign y = count_ones_out_r;
 
 endmodule
