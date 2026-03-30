55d141bc16a1891f1141f883c4a60871c6ac1625
SreeraaghavRaja-22 <sreeraaghav@gmail.com>
2026-03-30 01:13:58 -0400

    implemented a basic ram entity

--- rtl/threshold_ram.sv
+++ rtl/threshold_ram.sv
@@ -23,4 +23,34 @@ module ram_sdp_vivado2 #(
     localparam logic [MAX_STYLE_LEN*8-1:0] MEM_STYLE = string_as_logic_t'(STYLE);
 
     // used packed logic array in the attribute
-    (* ram_style = MEM_STYLE *) logic [DATA_WIDTH-1:0] ram[2]
+    (* ram_style = MEM_STYLE *) logic [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH]
+    logic [DATA_WIDTH-1:0] rd_data_ram; 
+
+    always_ff @(posedge clk) begin 
+        if (wr_en) ram[wr_addr] <= wr_data; 
+        if (rd_en) rd_data_ram  <= ram[rd_addr];
+    end
+
+    if(WRITE_FIRST) begin : l_write_first
+        logic bypass_valid_r = 1'b0; 
+        logic [DATA_WIDTH-1:0] bypass_data_r; 
+
+        always_ff @(posedge clk) begin 
+            if(rd_en && wr_en) bypass_data_r <= wr_data;
+            if (rd_en) bypass_valid_r <= wr_en && rd_addr == wr_addr; 
+        end
+
+        if(REG_RD_DATA) begin : l_reg_rd_data
+            always_ff @(posedge clk) if (rd_en) rd_data <= bypass_valid_r ? bypass_data_r : rd_data_ram; 
+        end else begin : l_no_reg_rd_data
+            assign rd_data = bypass_valid_r ? bypass_data_r : rd_data_ram; 
+        end 
+    end else begin : l_read_first 
+        if (REG_RD_DATA) begin : l_reg_rd_data
+            always_ff @(posedge clk) if (rd_en) rd_data <= rd_data_ram;
+        end else begin : l_no_reg_rd_data 
+            assign rd_data = rd_data_ram;
+        end 
+    end 
+endmodule  
+        
